36bedebe232af06fc1b9ed2c075b715bd356fbe8
Pablo <pagarc24@ucm.es>
2025-07-04 13:48:35 +0200

    Instalador de dependencias
    De aquellas que no vienen en la librería estándar de Python3

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,50 +1,41 @@
 #!/bin/bash
+[ "$UID" -ne 0 ] && echo "This script must be run as root. Trying again with sudo..." && exit 1
 
-PYTHON_PATH=""
 ANALYZER_SOURCE_CODE="analyzer.py"
 ANALYZER_EXECUTABLE="system_analyzer.sh"
 
-python_not_installed() {
-    VERSION="3.13.5"
-    PRODUCT="Python-$VERSION"
-    FILE="$PRODUCT.tar.xz"
-    URL="https://www.python.org/ftp/python/$VERSION/$FILE"
-
-    echo "Downloading $FILE from $URL..."
-
-    wget -O "$FILE" "$URL"
-
+dependencies() {
+    python3 -m pip install --break-system-packages -r dependencies.txt
     if [ $? -eq 0 ]; then
-        echo "Download completed successfully. The file was saved as $FILE"
+        echo "Dependencies installed"
     else
-        echo "Error: Download failed"
+        echo "Error: An error occurred while installing the dependencies"
         exit 1
     fi
-
-    echo "Extracting $FILE into $PRODUCT"
-
-    mkdir $PRODUCT
-    tar -xvf $FILE $PRODUCT
-
-    if [ $? -eq 0 ]; then
-        echo "Extraction completed successfull"
-    else
-        echo "Error: Extraction failed"
-        exit 1
-    fi
-
-    #TODO FINISH
 }
 
-#PYTHON
 command -v python3 &> /dev/null
 if [ $? -eq 0 ]; then
     PYTHON_PATH=$(which python3)
 else
-    python_not_installed
+    echo "Error: Python3 not installed in this system"
+    exit 1
 fi
 
-echo "$PYTHON_PATH $ANALYZER_SOURCE_CODE" > $ANALYZER_EXECUTABLE
+python3 -m pip --version
+if [ $? -eq 0 ]; then
+    dependencies
+else
+    python3 -c "import urllib.request; urllib.request.urlretrieve('https://bootstrap.pypa.io/get-pip.py', 'get-pip.py')"
+    python3 get-pip.py --break-system-packages
+    if [ $? -eq 0 ]; then
+        dependencies
+    else
+        echo "Error: pip not installed in this system"
+    fi
+    exit 1
+fi
+
+echo "$PYTHON_PATH $(pwd)/$ANALYZER_SOURCE_CODE" > $ANALYZER_EXECUTABLE
 chmod +x $ANALYZER_EXECUTABLE
-#ALMACENAR EN UNA VARIABLE DE ENTORNO PARA QUE SOLO TENGA QUE LLAMARSE EL COMANDO
-echo "Analyzer installed sucesfully. You can run the script using the command './$ANALYZER_EXECUTABLE'"
+echo "Analyzer installed sucesfully. You can run the script using the command ' sudo ./$ANALYZER_EXECUTABLE'"
